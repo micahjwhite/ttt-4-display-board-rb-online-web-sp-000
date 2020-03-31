685ee32a31e53a9350c4186073f7489f90eb2830
Micah White <micahjwhite@gmail.com>
2020-03-31 13:35:45 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -15,3 +15,12 @@ display_board(board)
 
 board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   | X |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+display_board(board)
