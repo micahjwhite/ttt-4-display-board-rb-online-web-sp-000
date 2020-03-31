24a8c0d5e0b1cb3bf462647d58ac9bfdd61e605c
Micah White <micahjwhite@gmail.com>
2020-03-31 13:44:32 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -18,7 +18,7 @@ board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 def display_board(board)
   puts "   |   |   "
   puts "-----------"
-  puts "   |   |   "
+  puts "   | X |   "
   puts "-----------"
   puts "   |   |   "
 end
