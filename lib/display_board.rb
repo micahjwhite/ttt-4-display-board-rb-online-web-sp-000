d408d8b10b1ef005abd557eae0f376529accc5a2
Micah White <micahjwhite@gmail.com>
2020-03-31 15:10:51 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,110 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+display_board(board)
+
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   | #{board[4]} |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+display_board(board)
+
+board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def display_board(board)
+  puts " #{board[0]} |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+display_board(board)
+
+board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
+
+def display_board(board)
+  puts " #{board[0]} |   |   "
+  puts "-----------"
+  puts "   | #{board[4]} |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+display_board(board)
+
+board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+display_board(board)
+
+board = [" ", " ", " ", " ", " ", " ", "O", "O", "O"]
+
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+display_board(board)
+
+board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
+
+def display_board(board)
+  puts " #{board[0]} |   |   "
+  puts "-----------"
+  puts "   | #{board[4]} |   "
+  puts "-----------"
+  puts "   |   | #{board[8]} "
+end
+
+display_board(board)
+
+board = [" ", " ", "O", " ", "O", " ", "O", " ", " "]
+
+def display_board(board)
+  puts "   |   | #{board[2]} "
+  puts "-----------"
+  puts "   | #{board[4]} |   "
+  puts "-----------"
+  puts " #{board[6]} |   |   "
+end
+
+display_board(board)
+
+board = ["X", "X", "X", "X", "O", "O", "X", "O", "O"]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+display_board(board)
