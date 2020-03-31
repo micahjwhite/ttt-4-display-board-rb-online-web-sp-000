a448d93b751783cca54ddfc1d6986c10bb4d9e91
Micah White <micahjwhite@gmail.com>
2020-03-31 13:28:19 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+board1 = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 
 def display_board(board)
   puts "   |   |   "
@@ -11,4 +11,4 @@ def display_board(board)
   puts "   |   |   "
 end
 
-display_board()
+display_board(board1)
